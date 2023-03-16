77161ee66452be32d963ff5f3356ccfcdbb6fc85
Kelvin <74870436+JRSKelvin@users.noreply.github.com>
2023-03-16 20:19:33 +0700

    Added All Previous Next Configuration With Extra Images Loader

--- next.config.js
+++ next.config.js
@@ -1,5 +1,27 @@
+// https://nextjs.org/docs/api-reference/next.config.js/cdn-support-with-asset-prefix
+// https://nextjs.org/docs/api-reference/next.config.js/basepath
+// https://nextjs.org/docs/api-reference/next.config.js/custom-image-loader-config
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+let assetPrefix = ""
+let basePath = ""
+let imagesLoader = undefined
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "")
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+  imagesLoader = {
+    loader: "akamai",
+    path: "/",
+  }
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: imagesLoader,
   reactStrictMode: true,
 }
 
