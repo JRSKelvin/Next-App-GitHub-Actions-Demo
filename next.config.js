83c15741a547db19072cb3d46f8cca15864dd883
Kelvin <74870436+JRSKelvin@users.noreply.github.com>
2023-03-16 20:12:35 +0700

    Remove Previous Next Configuration For Code Cleaning

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-let assetPrefix = ""
-let basePath = "/"
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "")
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig
