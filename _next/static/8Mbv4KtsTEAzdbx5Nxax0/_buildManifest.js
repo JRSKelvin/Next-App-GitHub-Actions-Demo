6e192f8f6546bcbf228315c59a4c396945868f38
JRSKelvin <74870436+JRSKelvin@users.noreply.github.com>
2023-03-18 08:55:29 +0000

    Deploying to gh-pages from @ JRSKelvin/Next-App-GitHub-Actions-Demo@d4534f1eff05453992d43e4f6fd349079f3cf599 🚀

--- _next/static/8Mbv4KtsTEAzdbx5Nxax0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/73c8490a373ee116.css","static/chunks/pages/index-47031a85ac54ef99.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/hUO_cUKI-WQtCE-oswYYE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/73c8490a373ee116.css","static/chunks/pages/index-fa506685a29b9ea9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
